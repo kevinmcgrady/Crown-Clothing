142c90db112dc84b30f72330533e8dc02582e706
kevinmcgrady <kevinmcgrady47@gmail.com>
2019-12-07 15:41:47 +0000

    started checkout page and added route

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import { Switch, Route, Redirect } from "react-router-dom";
 import HomePage from "./pages/homepage/homepage.component";
 import ShopPage from "./pages/shop/shop.component";
 import SignInAndSignUpPage from "./pages/sign-in-and-sign-up/sign-in-and-sign-up.component";
+import CheckoutPage from './pages/checkout/checkout.component';
+
 // import header.
 import Header from "./components/header/header.component";
 // Import firebase.
@@ -57,6 +59,7 @@ class App extends React.Component {
           <Route path="/" exact component={HomePage} />
           <Route path="/shop" component={ShopPage} />
           <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to='/' />) : (<SignInAndSignUpPage />)} />
+          <Route exact path="/checkout" component={CheckoutPage} />
         </Switch>
       </div>
     );
