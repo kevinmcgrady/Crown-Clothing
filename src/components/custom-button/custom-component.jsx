75509e147c9e84f2535a6c2eed2abe182d96789a
kevinmcgrady <kevinmcgrady47@gmail.com>
2019-12-27 12:18:59 +0000

    added js styles, fixed button styles and added redux devtools extension

--- src/components/custom-button/custom-component.jsx
+++ src/components/custom-button/custom-component.jsx
@@ -1,13 +1,21 @@
-import React from 'react';
-import './custom-button.styles.scss';
-import { CustomButtonContainer } from './custom-button.styles';
+import React from "react";
 
-const CustomButton = ({ children, ...props }) => {
-    return (
-        <CustomButtonContainer { ...props }>
-            { children }
-        </CustomButtonContainer>
-    )
-}
+import "./custom-button.styles.scss";
+
+const CustomButton = ({
+  children,
+  isGoogleSignIn,
+  inverted,
+  ...otherProps
+}) => (
+  <button
+    className={`${inverted ? "inverted" : ""} ${
+      isGoogleSignIn ? "google-sign-in" : ""
+    } custom-button`}
+    {...otherProps}
+  >
+    {children}
+  </button>
+);
 
 export default CustomButton;
--- src/redux/store.js
+++ src/redux/store.js
@@ -1,12 +1,13 @@
 import { createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore } from 'redux-persist';
 
 import rootReducer from './root-reducer';
 
 const middlewares = [logger];
 
-const store = createStore(rootReducer, applyMiddleware(...middlewares));
+const store = createStore(rootReducer, composeWithDevTools(applyMiddleware(...middlewares)));
 const persistor = persistStore(store);
 
 export { store, persistor };
